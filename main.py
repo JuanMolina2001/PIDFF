f935efa8530962c06a7239f2c0209635f5736496
JuanMolina2001 <juanmolinajm2001@gmail.com>
2024-09-30 01:58:36 -0300

    add set option

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import terminal
 print(terminal.DNA)
 dirname = os.path.dirname(__file__)
 win = tk.Tk()
-win.title("Bioinformatica")
+win.title("PIDFF")
 win.geometry("800x600")
 # form 
 tk.Label(win, text="Enter your email:").pack()
@@ -18,6 +18,11 @@ email.pack()
 tk.Label(win, text="Enter the IDs of the proteins:").pack()
 codes = tk.Text(win, height=10, width=50)
 codes.pack()
+# images
+delete_img = ImageTk.PhotoImage(Image.open(os.path.join(dirname, "assets/images/delete.png")).resize((30, 30)))
+add_img = ImageTk.PhotoImage(Image.open(os.path.join(dirname, "assets/images/add.png")).resize((30, 30)))
+export_img = ImageTk.PhotoImage(Image.open(os.path.join(dirname, "assets/images/excel.png")).resize((30, 30)))
+set_img = ImageTk.PhotoImage(Image.open(os.path.join(dirname, "assets/images/set.png")).resize((30, 30)))
 # If you want more information about proteins, add the field names here 
 # Si quieres más información sobre las proteínas, agrega los nombres de los campos aquí
 columns = ("ID", "organism", "DBSOURCE", "sequence")
@@ -29,9 +34,11 @@ for col in columns:
     index += 1
 
 # commands for buttons
-def submit():
+def submit(add:bool):
     all_codes = codes.get("1.0", tk.END).split("\n")
     codes.delete("1.0", tk.END)
+    if add is False:
+        delete()
     for code in all_codes:
         Entrez.email = email.get()
         # ID
@@ -61,27 +68,23 @@ def export():
         data.append(table.item(row)['values'])
     df = pd.DataFrame(data, columns=columns)
     df.to_excel(filename, index=False)
+def set_table():
+    submit(False)
+def add_table():
+    submit(True)
 # submit button
-submit_img = ImageTk.PhotoImage(
-    Image.open(os.path.join(dirname, "assets/images/add.png")).resize((30, 30))
-)
-
-submit_btn =tk.Button(win, image=submit_img, command=submit)
-submit_btn.pack()
+frame_form = tk.Frame(win)
+set_btn = tk.Button(frame_form, image=set_img, command=set_table)
+add_btn =tk.Button(frame_form, image=add_img, command=add_table)
+set_btn.pack( side="left")
+add_btn.pack(side="right")
+frame_form.pack()
 # buttons for the table
-frame_btn = tk.Frame(win)
-delete_img = ImageTk.PhotoImage(
-    Image.open(os.path.join(dirname, "assets/images/delete.png")).resize((30, 30))
-)
-
-delete_btn = tk.Button(frame_btn, image = delete_img, command=delete)
-export_img = ImageTk.PhotoImage(
-    Image.open(os.path.join(dirname, "assets/images/excel.png")).resize((30, 30))
-)
-
-export_btn = tk.Button(frame_btn, image = export_img, command=export)
+frame_table = tk.Frame(win)
+delete_btn = tk.Button(frame_table, image = delete_img, command=delete)
+export_btn = tk.Button(frame_table, image = export_img, command=export)
 table.pack(expand=True, fill="both")
 delete_btn.pack(side="right")
 export_btn.pack(side="left")
-frame_btn.pack()
+frame_table.pack()
 win.mainloop()
